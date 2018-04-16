62c97a0518a2a4bd41a2bee7955bdfa033acb135
wilfridociprianosantoslopez <wilys9510@gmail.com>
2018-04-16 12:19:10 -0500

    update wiiiz

--- vistas/baseIndex.php
+++ vistas/baseIndex.php
@@ -1,5 +1,5 @@
 <?php
-include "../Controlador/sesion.php";
+include "../controlador/sesion.php";
   //print_r( $_SESSION[session_name()]+" abcde123
   //echo $_SESSION["usuario"] . ' => rol '.$_SESSION["rol"];
 
@@ -7,16 +7,16 @@ include "../Controlador/sesion.php";
     //echo"<script language='javascript'>alert( 'autentificado baseIndex=> rol ' +  ". $_SESSION['rol'].")</script>";
 
   if($_SESSION["autentificado"] == true){
-  if ($_SESSION["rol"] == 1) {//ROL -2- => COORDINADOR
-      //header("Location: Administrador/index.php");
+  if ($_SESSION["rol"] == 1) {//ROL -1- => ADMINISTRADOR
+      //header("Location: administrador/index.php");
       echo'<script language="javascript">window.location="administrador/index.php"</script>';
   }
   if ($_SESSION["rol"] == 2) {//ROL -2- => COORDINADOR
-      //header("Location: Coordinador/index.php");
-        echo'<script language="javascript">window.location="coordinador/index.php"</script>';
+      //header("Location: coordinador/index.php");
+      echo'<script language="javascript">window.location="coordinador/index.php"</script>';
   }
   if ($_SESSION["rol"] == 3) {//ROL -3- => DEFENSOR
-    //header("Location: Defensor/index.php");
+    //header("Location: defensor/index.php");
       echo'<script language="javascript">window.location="defensor/index.php"</script>';
   }
   if ($_SESSION["rol"] == 4) {//ROL -3- => DEFENSOrCoordinador
