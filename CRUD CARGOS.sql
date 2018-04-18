17f4f0e3bb42f46099228006f275f3ae9bfc409d
wilfridoSantos <wilys9510@gmail.com>
2018-04-18 13:26:09 -0500

    Add files via upload

---
+++ CRUD CARGOS.sql
@@ -0,0 +1,13 @@
+SELECT * FROM bddefensoria.cargo;
+-- INSERT
+INSERT INTO cargo VALUES (1, 'admin');
+INSERT INTO cargo VALUES (2, 'coordinador');
+INSERT INTO cargo VALUES (3, 'coordinador_defensor');
+INSERT INTO cargo VALUES (4, 'estadistica');
+INSERT INTO cargo VALUES (5, 'defensor');
+
+-- UPDATE
+
+-- DELETE
+
+
