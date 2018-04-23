fd531378a4783c63320a25e5d6897e7fc0444ef2
wilfridociprianosantoslopez <wilys9510@gmail.com>
2018-04-23 14:40:19 -0500

    23 2 : 40

--- recursos/js/Gestion.js
+++ recursos/js/Gestion.js
@@ -2,46 +2,45 @@ $(document).ready(function () {
 	var informeA=document.getElementById('informeAnual');
 		informeA.addEventListener('click', informeAnual, false);
 		function informeAnual() {
-			$('#menuContainer').load("../../vistas/coordinador/informeAnual.html");
+			$('#menuContainer').load("informeAnual.html");
 		};
 
 	var informeM=document.getElementById('informeMensual');
 		informeM.addEventListener('click', informeMensual, false);
 		function informeMensual() {
-				$('#menuContainer').load("../../vistas/coordinador/informeMensual.html");
+				$('#menuContainer').load("informeMensual.html");
 		};
 
 	var informeE=document.getElementById('informeEspecifico');
 		informeE.addEventListener('click', informeEspecifico, false);
 		function informeEspecifico() {
-				$('#menuContainer').load("../../vistas/coordinador/informeEspecifico.html");
+				$('#menuContainer').load("informeEspecifico.html");
 		};
 
 	var tarjetaI=document.getElementById('tarjetaInformativa');
 		tarjetaI.addEventListener('click', tarjetaInformativa, false);
 		function tarjetaInformativa() {
-			$('#menuContainer').load("../../vistas/coordinador/tarjetaInformativa.html");
+			$('#menuContainer').load("tarjetaInformativa.html");
 		};
 		
 	var listarDef=document.getElementById('listarDefensores');
 		listarDef.addEventListener('click', listaDefensores, false);
 		function listaDefensores() {
-			console.log("ACCESO A FUNCION listaDefensor ahdka");
-			$('#menuContainer').load("../../vistas/coordinador/listarDefensores.php");
+			//console.log("ACCESO A FUNCION listaDefensor ahdka");
+			$('#menuContainer').load("listarDefensores.php");
 		//window.location="../../controlador/defensor/controladorListaDef.php"
 	};
 
 	var listarAudiencia=document.getElementById('listarAudiencias');
 	listarAudiencia.addEventListener('click', listarAudiencias, false);
 	function listarAudiencias() {
-		$('#menuContainer').load("../../vistas/coordinador/listarAudiencias.php");
+		$('#menuContainer').load("listarAudiencias.php");
 		//window.location="../../controlador/defensor/controladorListaDef.php"
 	};
-
 	var listarVisitas=document.getElementById('listarVisitas');
 	listarVisitas.addEventListener('click', listarVisitas, false);
 	function listarVisitas() {
-		$('#menuContainer').load("../../vistas/coordinador/listarAudiencias.php");
+		$('#menuContainer').load("listarVisitas.php");
 		//window.location="../../controlador/defensor/controladorListaDef.php"
 	};
 
@@ -49,36 +48,7 @@ $(document).ready(function () {
 	registrar.addEventListener('click', regDefensores, false);
 	function regDefensores() {
 		//alert('sfafa');
-		$('#menuContainer').load("../../vistas/usuarios/registrar.html");
+		$('#menuContainer').load("../usuarios/registrar.html");
 	};
-
-
-
-
-
-	// Datepicker Popups calender to Choose date.
-	/*$(function() {
-		$("#datepicker").datepicker();
-		// Pass the user selected date format.
-		$("#format").change(function() {
-			$("#datepicker").datepicker("option", "dateFormat", $(this).val());
-		});
-	});*/
-
-
-	
-/* 
-
-	var listar=document.getElementById('listarDefensores');
-			listar.addEventListener('click',listDefensores,false);
-			function listDefensores(){
-			 $('#menuContainer').load("../../vistas/coordinador/listarDefensores.php");
-			}; */
-/* 	var infor=document.getElementById('informacionDefensor');
-			infor.addEventListener('click',infoDefensores,false);
-			function infoDefensores(){
-			 $('#menuContainer').load("../../vistas/coordinador/verInfoDefensor.php");
-			}; */
-
 });
 
