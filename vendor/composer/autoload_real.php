bd4f109a7d860b2226687bb2d5ed40394621a8ec
wilfridociprianosantoslopez <wilys9510@gmail.com>
2018-06-19 09:04:03 -0500

    add composer json

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,24 +47,6 @@ class ComposerAutoloaderInitd431367ecf06c09c8a4cf6b46ef1cbea
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInitd431367ecf06c09c8a4cf6b46ef1cbea::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequired431367ecf06c09c8a4cf6b46ef1cbea($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequired431367ecf06c09c8a4cf6b46ef1cbea($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
